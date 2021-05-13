e5d0d83a1c6b70f5f63db4fbf3c2f2631b41510c
tavo <gasb_150@hotmail.com>
2021-05-13 17:08:01 -0500

    Add number of products by category

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,88 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Category.create!(
+  name: 'Alimentos',
+)
+
+Category.create!(
+  name: 'Health',
+)
+
+Category.create!(
+  name: 'Electronics',
+)
+
+Category.create!(
+  name: 'Cleaning',
+)
+
+Category.create!(
+  name: 'Office',
+)
+
+Product.create!(
+  name: "Arroz",
+  description: "",
+  category_id: 1,
+  iva:0,
+  price:1800,
+)
+
+Product.create!(
+  name: "Frijol",
+  description: "",
+  category_id: 1,
+  iva:0,
+  price:1000,
+)
+
+Product.create!(
+  name: "Azucar",
+  description: "",
+  category_id: 1,
+  iva:5,
+  price:2200,
+)
+
+Product.create!(
+  name: "Cafe",
+  description: "",
+  category_id: 1,
+  iva:5,
+  price:2200,
+)
+
+Product.create!(
+  name: "Chocolate",
+  description: "",
+  category_id: 1,
+  iva:5,
+  price:2200,
+)
+
+Product.create!(
+  name: "Sal",
+  description: "",
+  category_id: 1,
+  iva:0,
+  price:2200,
+)
+
+Product.create!(
+  name: "Televisor",
+  description: "",
+  category_id: 3,
+  iva:0,
+  price:2200,
+)
+
+Product.create!(
+  name: "Celular",
+  description: "",
+  category_id: 3,
+  iva:0,
+  price:2200,
+)
