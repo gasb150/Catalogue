92eec8af5d8182e0153cfca9f8ac9908ed4f650e
tavo <gasb_150@hotmail.com>
2021-05-13 15:38:02 -0500

    Add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   resources :categories
   resources :products
+  devise_for :users
+  #, :controllers => {registrations: 'registrations'}
+  # get 'users', to: 'categories#index'
+  # get 'users/sing_up', to: 'users#new'
+  # devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'products#index'
 end
