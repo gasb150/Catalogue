e66ef95b95d6ab9be081fbba61ee8d1f4af21a51
tavo <gasb_150@hotmail.com>
2021-05-13 14:15:53 -0500

    Add categories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_13_190305) do
+ActiveRecord::Schema.define(version: 2021_05_13_191422) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
