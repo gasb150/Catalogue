5e58bc1ed29f0ccee7f100021417a428442f1761
tavo <gasb_150@hotmail.com>
2021-05-15 01:57:07 -0500

    Add validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ApplicationRecord
   belongs_to :category
+
+  validates :name, presence:true
+  validates :description, presence:true
+  validates :category_id, presence:true
+  validates :price, presence:true
 end
