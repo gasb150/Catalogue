b62559ff40c6cf486859ea2edab88a79060f1de3
tavo <gasb_150@hotmail.com>
2021-05-14 20:09:10 -0500

    Style icons

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,6 +1,6 @@
 module ProductsHelper
  def showImage(product)
-  el = "<img src='https://source.unsplash.com/800x600/?{#{product.name}}' alt=#{product.name} >"
-  el.html_safe
+  # el = "<img src='https://source.unsplash.com/800x600/?{#{product.name}}' alt=#{product.name} >"
+  # el.html_safe
 end
 end
