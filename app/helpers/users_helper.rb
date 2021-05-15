fee8a3932ad5612966fb5f9cf0963c59a2acf324
tavo <gasb_150@hotmail.com>
2021-05-15 17:06:48 -0500

    Fix linters

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,10 @@
 module UsersHelper
   def username
-    if current_user.nil?
-      el = ''
-    else
-      el = current_user.username
-    end
+    el = if current_user.nil?
+           ''
+         else
+           current_user.username
+         end
     el.html_safe
   end
 end
